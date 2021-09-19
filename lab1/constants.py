3275563db6fee8fd380fef7508b5e3f9d88130d3
b1est <bondarenko2001@gmail>
2021-09-19 10:05:32 +0300

    Geffe (not done) and main class Generator

---
+++ lab1/constants.py
@@ -0,0 +1,22 @@
+def count_mask(n):
+    res = 0
+    for i in range(n):
+        res += 2 ** i
+    return res
+
+n = {
+    "l11": 11,
+    "l9": 9,
+    "l10": 10
+    }
+taps = {
+    "l11": (0, 2),
+    "l9": (0, 1, 3, 4),
+    "l10": (0, 3)
+    }
+
+masks = {
+    n["l11"]: count_mask(n["l11"]),
+    n["l9"]: count_mask(n["l9"]),
+    n["l10"]: count_mask(n["l10"])
+    }
