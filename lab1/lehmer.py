0ac8b9bb22e0322698f116a29dc8929c6d6d3070
b1est <bondarenko2001@gmail>
2021-10-24 16:15:52 +0300

    Tests

--- lab1/lehmer.py
+++ lab1/lehmer.py
@@ -1,4 +1,3 @@
-import numpy as np
 from math import ceil
 
 class Lehmer:
@@ -7,17 +6,17 @@ class Lehmer:
         self.m = const[0]
         self.a = const[1]
         self.c = const[2]
-        self.x = np.array([const[3]])
+        self.x = [const[3]]
 
     def calc(self, n):
         if n % 8 == 0 and n != 8:
             while n > 0 and n != 0 and n != 8:
-                self.x = np.append(self.x, [(self.a*self.x[-1]+self.c)%self.m])
+                self.x.append((self.a*self.x[-1]+self.c)%self.m)
                 n-=8
         else:
             n = ceil(n/8)*8
             while n > 0 and n != 0 and n != 8: 
-                self.x = np.append(self.x, [(self.a*self.x[-1]+self.c)%self.m])
+                self.x.append((self.a*self.x[-1]+self.c)%self.m)
                 n-=8
         return self.x
 
@@ -29,29 +28,23 @@ class LehmerLow (Lehmer):
 
     def generate_bits(self, n):
         Lehmer.calc(self, n)
-        res = 0
-        first_len = len(bin(int(self.x[0]) & self.mask))-2
+        res = 0        
         for i in self.x:
             res = (res << 8) | (int(i) & self.mask)
-        return res, first_len
-        
-        
-
-
+        return res
         
 class LehmerHigh(Lehmer):
 
     def __init__(self, const):
         Lehmer.__init__(self, const)
-        self.mask = 0xff
+        self.mask = 0xff000000
         
     def generate_bits(self, n):
         Lehmer.calc(self, n)  
         res = 0
-        first_len = len(bin(int(self.x[0]) & self.mask))-2
         for i in self.x:
-            res = (res << 8) | (int(i) >> 24)
-        return res, first_len
+            res = (res << 8) | ((int(i) & self.mask) >> 24)
+        return res
 
 
 
--- lab1/main.py
+++ lab1/main.py
@@ -4,10 +4,10 @@ from geffe import Geffe, GeffeRegister, Lfsr
 from lehmer import LehmerLow, LehmerHigh
 from wolfram import Wolfram
 from multiprocessing import Pool
-import argparse
+from math import pow, sqrt
+from scipy.stats import norm
 
 class Generator:
-
     def __init__(self, length):
         self.length_of_bit_sequence = length
         self.geffe_ = Geffe(
@@ -20,101 +20,109 @@ class Generator:
         self.l20_ = Lfsr(randint(1, count_mask(n['l20'])), taps['l20'], n['l20'])
         self.l89_ = Lfsr(randint(1, count_mask(n['l89'])), taps['l89'], n['l89'])
         self.wolfram_ = Wolfram(32)
+        for txt in ('geffe.txt', 'l20.txt', 'l89.txt', 'lehmerhigh.txt', 'lehmerlow.txt', 'wolfram.txt'):
+            f = open(txt, 'w')
+            f.close()
 
     def geffe(self):
         geffe_ = self.geffe_.generate_bits(self.length_of_bit_sequence)
-        geffe_len = len(bin(geffe_)) - 2
-        print((self.length_of_bit_sequence - geffe_len)*'0'+bin(geffe_)[2:])
+        with open("geffe.txt", "a") as f:
+            for i in range(self.length_of_bit_sequence):
+                f.write(str((geffe_>>i) & 1))
+        print('Geffe done!')
 
     def lehmer_low(self):
-        lehmer_low_, first_block = self.lehmer_low_.generate_bits(self.length_of_bit_sequence)
-        lehmer_low_len = len(bin(lehmer_low_))-2
-        first_block_len = len(bin(first_block))-2
-        if first_block_len == 8:
-            if lehmer_low_len == self.length_of_bit_sequence:
-                print(bin(lehmer_low_)[2:])
-            else:
-                lehmer_low_ >>= abs((self.length_of_bit_sequence-lehmer_low_len))
-                print(bin(lehmer_low_)[2:])
-        else:
-            if lehmer_low_len == self.length_of_bit_sequence:
-                lehmer_low_ >>= abs((8-first_block_len))
-                
-                lehmer_low_= '0'+bin(lehmer_low_)[2:]
-                while len(lehmer_low_) != self.length_of_bit_sequence:
-                    lehmer_low_ = '0' + lehmer_low_
-                print( lehmer_low_)
-            elif lehmer_low_len < self.length_of_bit_sequence:
-                lehmer_low_= '0'+bin(lehmer_low_)[2:]
-                while len(lehmer_low_) != self.length_of_bit_sequence:
-                    lehmer_low_ = '0' + lehmer_low_
-                print( lehmer_low_)
-            else:
-                lehmer_low_ >>= abs(abs(self.length_of_bit_sequence-lehmer_low_len)+8-first_block_len)
-                lehmer_low_= '0' + bin(lehmer_low_)[2:]
-                while len(lehmer_low_) != self.length_of_bit_sequence:
-                    lehmer_low_ = '0' + lehmer_low_
-                print( lehmer_low_)
+        lehmer_low_ = self.lehmer_low_.generate_bits(self.length_of_bit_sequence)
+        with open("lehmerlow.txt", "a") as f:
+            for i in range(self.length_of_bit_sequence):
+                f.write(str((lehmer_low_>>i) & 1))
+        print('LehmerLow done!')
         
     def lehmer_high(self):
-        lehmer_high_, first_block = self.lehmer_high_.generate_bits(self.length_of_bit_sequence)
-        lehmer_high_len = len(bin(lehmer_high_))-2
-        first_block_len = len(bin(first_block))-2
-        if first_block_len == 8:
-            if lehmer_high_len == self.length_of_bit_sequence:
-                print( bin(lehmer_high_)[2:])
-            else:
-                lehmer_high_ >>= abs((self.length_of_bit_sequence-lehmer_high_len))
-                print( bin(lehmer_high_)[2:])
-        else:
-            if lehmer_high_len == self.length_of_bit_sequence:
-                lehmer_high_ >>= abs((8-first_block_len))
-                
-                lehmer_high_= '0'+bin(lehmer_high_)[2:]
-                while len(lehmer_high_) != self.length_of_bit_sequence:
-                    lehmer_high_ = '0' + lehmer_high_
-                print( lehmer_high_)
-            elif lehmer_high_len < self.length_of_bit_sequence:
-                lehmer_high_= '0'+bin(lehmer_high_)[2:]
-                while len(lehmer_high_) != self.length_of_bit_sequence:
-                    lehmer_high_ = '0' + lehmer_high_
-                print( lehmer_high_)
-            else:
-                lehmer_high_ >>= abs(abs(self.length_of_bit_sequence-lehmer_high_len)+8-first_block_len)
-                lehmer_high_= '0' + bin(lehmer_high_)[2:]
-                while len(lehmer_high_) != self.length_of_bit_sequence:
-                    lehmer_high_ = '0' + lehmer_high_
-                print( lehmer_high_)
+        lehmer_high_ = self.lehmer_high_.generate_bits(self.length_of_bit_sequence)
+        with open("lehmerhigh.txt", "a") as f:
+            for i in range(self.length_of_bit_sequence):
+                f.write(str((lehmer_high_>>i) & 1))
+        print('LehmerHigh done!')
+ 
 
     def l20(self):
         l20_ = self.l20_.generate_bits(self.length_of_bit_sequence)
-        l20_len = len(bin(l20_)) - 2
-        print( (self.length_of_bit_sequence - l20_len)*'0'+bin(l20_)[2:])
+        with open("l20.txt", "a") as f:
+            for i in range(self.length_of_bit_sequence):
+                f.write(str((l20_>>i) & 1))
+        print('L20 done!')
     
     def l89(self):
         l89_ = self.l89_.generate_bits(self.length_of_bit_sequence)
-        l89_len = len(bin(l89_)) - 2
-        print( (self.length_of_bit_sequence - l89_len)*'0'+bin(l89_)[2:])
+        with open("l89.txt", "a") as f:
+            for i in range(self.length_of_bit_sequence):
+                f.write(str((l89_>>i) & 1))
+        print('L89 done!')
 
     def wolfram(self):
         wolfram_ = self.wolfram_.generate_bits(self.length_of_bit_sequence)
-        wolfram_len = len(bin(wolfram_)) - 2
-        print( (self.length_of_bit_sequence - wolfram_len)*'0'+bin(wolfram_)[2:])
+        with open("wolfram.txt", "a") as f:
+            for i in range(self.length_of_bit_sequence):
+                f.write(str((wolfram_>>i) & 1))
+        print('Wolfram done!')
+
+
+
 
 def results_of_generators(generator: Generator,  processes: int):
         funcs = (generator.geffe, generator.lehmer_low, generator.lehmer_high, generator.l20, generator.l89, generator.wolfram)
         pool = Pool(processes=processes)
         sub_processes = []
-    
         for func in funcs:
             r = pool.apply_async(func)
             sub_processes.append(r)
         pool.close()
         pool.join()
 
-if __name__ == "__main__":  
-    generator = Generator(10**6)
-    results_of_generators(generator, 4)
+
+class Tests: 
+    def __init__(self):
+        self.txt_names = ('geffe.txt', 'l20.txt', 'l89.txt', 'lehmerhigh.txt', 'lehmerlow.txt', 'wolfram.txt')
+        self.alphas = (0.01, 0.05, 0.1)
+        
+
+    def uniformityTest(self):
+        f = open('geffe.txt', "r").read()
+        r = 16
+        m2 = len(f) // r
+        n = m2*r
+        hi2 = 0
+        l = 255*(r-1)
+        f_ = [f[x:x+m2] for x in range (0, len(f), m2)]
+        for i in range(0, 256):
+            vi = len([i for x in range(0, len(f), 8) if int(f[x:x+8], 2) == i])
+            for j in range(r):
+                vi2 = pow(len([i for x in range(0, len(f_[j]), 8) if int(f_[j][x:x+8], 2) == i]), 2)
+                hi2 += (vi2 / (vi * m2))
+                
+        print(hi2)
+        hi2 = n*(hi2 - 1)
+        print(f'hi2 = {hi2}')
+        for alpha in self.alphas:
+            hi2teor = sqrt(2*l)*norm.ppf(1-alpha)+l    
+            print(f'hi2(1-{alpha}) = {hi2teor}')
+            if hi2 <= hi2teor:
+                print('ok')
+            else:
+                print('ne ok')
+
+
+        
+
+
+if __name__ == "__main__":
+    
+    # generator = Generator(10**6)
+    # results_of_generators(generator, 4)
+
+    test = Tests()
+    test.uniformityTest()
     
     
     
